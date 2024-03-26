0f15c0db610bcbe03c58f15ca6b38aa9fa41c18a
deivid1801 <deividleonardo443@gmail.com>
2024-03-26 14:57:26 -0300

    leo

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,86 @@
 <script setup>
-  
-  import { ref, onMounted } from 'vue'
-
-const contador = ref(0)
-
-function incrementarContador() {
-  contador.value++
-}
-function decrementarContador() {
-  contador.value--
-}
-
-onMounted(() => {
-  console.log(`O valor inicial do contador é ${contador.value}.`)
-})  
-const rawHtml = '<span style="color: red">Este é um texto em vermelho</span>'
-function reiniciarContador(){
-  contador.value = 0
-}
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <button @click="incrementarContador">Incrementar</button>
-  <button @click="reiniciarContador">Reiniciar</button>
-  <button @click="decrementarContador">Decrementar</button>
-  <p>Valor do contador é: {{ contador }}</p>
-<p>Usando interpolação de textos: {{rawHtml}}</p>
-    <p>Usando v-html: <span v-html="rawHtml"></span></p>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/contact">Contact</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
 </template>
 
 <style scoped>
-button {
-  font-weight: bold;
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
 }
 </style>
